8818ab7a747e2be1dfb02ce6ebd38df5e4b31027
Ethan <elnair122@gmail.com>
2023-03-30 07:59:52 -0400

    video 1 done

--- video1/tutorial.py
+++ video1/tutorial.py
@@ -1,13 +1,20 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def home():
     # you can write HTML in this string, or some text by itself and it will be displayed 
     return "Hello! this is the main page <h1>HELLO</h1>"
 
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}"
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))
+# home is name of function we want to run
 
 if __name__ == '__main__':
     app.run()
