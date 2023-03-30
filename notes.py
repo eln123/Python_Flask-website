ab1054b192f6b25f2c50adaf2618f515b777654b
Ethan <elnair122@gmail.com>
2023-03-30 06:23:00 -0400

    first commit

---
+++ notes.py
@@ -0,0 +1,6 @@
+# flask is a micro web framework for building websites with Python
+# usually used as a back end and then another front is connected using a restful api
+
+# flask is a micro framework, instead of a full web framework
+# which also means it doesn't include a lot of the Django tools, like authentication
+
