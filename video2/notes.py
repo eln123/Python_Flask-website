1367593a4a0ecf7cdb47868200167119adbcf7e4
Ethan <elnair122@gmail.com>
2023-03-30 13:43:29 -0400

    done with video2

---
+++ video2/notes.py
@@ -0,0 +1,7 @@
+# this video is about templates and how to use your own HTML, CSS, and Javascript with this Python backend
+
+# make sure the folder for html files is called 'templates'
+# so that render_template works
+    # @app.route('/')
+    # def home():
+    #     return render_template('index.html')
--- video2/tutorial.py
+++ video2/tutorial.py
@@ -1,10 +1,10 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello! this is the main page <h1>HELLO</h1>"
+    return render_template('index.html', content = "this is the content prop", listOfStuff = ['pizza', 'hello', 'ice cream'])
 
 @app.route("/<name>")
 def user(name):
@@ -13,6 +13,7 @@ def user(name):
 @app.route("/admin")
 def admin():
     return redirect(url_for("home"))
+
 
 if __name__ == '__main__':
     app.run()
