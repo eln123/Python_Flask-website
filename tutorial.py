41a8659cb53e2fc19f6d31b3f9d6d2da3323cb65
Ethan <elnair122@gmail.com>
2023-03-30 06:58:27 -0400

    app created and running

--- tutorial.py
+++ tutorial.py
@@ -1,1 +1,15 @@
 from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def home():
+    # you can write HTML in this string, or some text by itself and it will be displayed 
+    return "Hello! this is the main page <h1>HELLO</h1>"
+
+
+if __name__ == '__main__':
+    app.run()
+
+# this is all you have to do to start a website and get it running
