f691e600d7b02cd369d90a5241d8502c48a63da6
Ethan <elnair122@gmail.com>
2023-03-30 14:50:02 -0400

    bootstrap-added

--- video3/notes.py
+++ video3/notes.py
@@ -11,3 +11,15 @@
 
 # notice app.run(debug=True)
 # this makes it so if you make changes, you don't have to run the file again, just refresh
+
+#__________
+
+# bootstrap
+    # bootstrap is basically a CSS framework for styling your website
+    # link: https://getbootstrap.com/docs/4.3/getting-started/introduction/
+    # copy and paste the CSS link there into your head tag in your base.html:
+        # <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    # copy and paste script tags for Javascript, you need those for their components to function
+        # ^ copy and paste script tags at bottom of body
+    # this is on a cdn, which basically means it grabs the CSS code and Javascript code from the internet, so we don't need to run any files
