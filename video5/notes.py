d85465735d8272ba5e5ec03ffce1f9b79dade7af
Ethan <elnair122@gmail.com>
2023-03-30 20:56:54 -0400

    session and secret key created

---
+++ video5/notes.py
@@ -0,0 +1,13 @@
+# video 5 is about sessions
+# how we can do the previous video's code better
+
+# a session is something you use while user is on website, then when you leave it disappears
+# ex: a user logs in, a new session is created
+# as they go through pages, they have the session data
+# with the user's info and other info
+# as soon as the user leaves, all the session data is gone
+# when they login back in, the session data reloads
+# sessions are temporary, stored on the server, not the client side
+# they are designed as a quick way to access information and to pass things around our server
+
+# import session from flask
