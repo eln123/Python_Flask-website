a8ac032c30559090dffca42e20a56b98ef01c165
Ethan <elnair122@gmail.com>
2023-04-02 20:33:41 -0400

    video8 done

---
+++ video8/notes.py
@@ -0,0 +1,12 @@
+# video 8 is about Adding, Deleting, and Updating users with SQLALchemy
+
+
+# ADD
+    # db.session.add(usr)
+    #        
+    #         db.session.commit()
+
+
+# DELETE
+# found_user = users.query.filter_by(name = user).delete()
+# db.commit()
